dcd302c93a6fc6258bf38bf1ca6618eb7b2fdc24
Olivia Chang <cozymonster29@gmail.com>
2015-06-24 16:42:56 -0400

    Finish log-in-log-out

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150502220754) do
+ActiveRecord::Schema.define(version: 20150614215953) do
 
   create_table "users", force: true do |t|
     t.string   "name"
