fbec33a5d722be7d3120e4478e77de9b08122687
Olivia Chang <cozymonster29@gmail.com>
2015-07-04 17:26:57 -0700

    Add password resets, also configured emails in production

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
 	# Change email to downcase
 	before_save :downcase_email
   # Create activation digest before save
@@ -47,6 +47,7 @@ class User < ActiveRecord::Base
     update_attribute(:remember_digest, nil)
   end
 
+  #ACTIVATION
   def activate
     update_attribute(:activated, true)
     update_attribute(:activated_at, Time.zone.now)
@@ -54,6 +55,22 @@ class User < ActiveRecord::Base
 
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+  #PASSWORD RESET
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest, User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  # Returns true if a password reset has expired.
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
   end
 
 
